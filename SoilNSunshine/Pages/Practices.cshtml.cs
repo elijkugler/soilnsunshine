3e0ddba3029cbcc9246c7f7b191270e9c6108a39
elijkugler <eli.j.kugler@outlook.com>
2022-03-22 15:32:43 -0500

    Got the parallax going, and the nav bar

---
+++ SoilNSunshine/Pages/Practices.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SoilNSunshine.Pages
+{
+    public class PracticesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ SoilNSunshine/Pages/shop.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SoilNSunshine.Pages
+{
+    public class shopModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
